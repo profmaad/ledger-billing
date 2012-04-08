1514c3ee8003a3962c57c403c2c01d15b828d05d
profmaad <prof.maad@lambda-bb.de>
2012-04-08 18:21:17 +0200

    implemented customer overview

--- ledger-billing.rb
+++ ledger-billing.rb
@@ -4,6 +4,8 @@ require 'json'
 require 'yaml'
 require 'haml'
 require 'data_mapper'
+require 'uri'
+require 'net/http'
 
 require 'pp'
 
@@ -22,6 +24,7 @@ class LedgerBilling < Sinatra::Base
 
   configure do |c|
     config = YAML.load_file(CONFIG_FILE)
+    config = {} if (config.nil? or !config)
     puts "Failed to load config file" if config.nil?
 
     config.each do |key,value|
@@ -60,5 +63,62 @@ class LedgerBilling < Sinatra::Base
     @@preferences = params
 
     redirect url("/preferences")
+  end
+
+  get "/customer" do
+    billables_account = construct_account_name(@@preferences["accounts"]["billable"])
+    receivables_account = construct_account_name(@@preferences["accounts"]["receivable"])
+
+    balances = ledger_rest_do_request("balance", "-E --flat "+billables_account+" "+receivables_account)
+
+    customers = {}
+    balances["accounts"].each do |account, balance|
+      if account.start_with?(billables_account)
+        customer_name = account[billables_account.length+1..-1]
+        type = :billable
+      elsif account.start_with?(receivables_account)
+        customer_name = account[receivables_account.length+1..-1]
+        type = :receivable
+      end
+
+      customers[customer_name] = {} if customers[customer_name].nil?
+
+      case type
+      when :billable
+        customers[customer_name][:billable] = balance
+      when :receivable
+        customers[customer_name][:receivable] = balance
+      end
+    end
+    @customers = customers
+    
+    @page_title = "Customers"
+    haml :customer_list
+  end
+
+  helpers do
+    def http_get_with_redirection(url)
+      response = Net::HTTP.get_response(url)
+      case response
+      when Net::HTTPSuccess
+        return response.body
+      when Net::HTTPRedirection
+        return http_get_with_redirection(URI.parse(response['location']))
+      else
+        response.error!
+      end
+    end
+
+    def ledger_rest_do_request(resource, query)
+      return JSON.parse(http_get_with_redirection(URI.parse("#{settings.ledger_rest_uri}/#{resource}?query=#{URI.escape(query)}")))
+    end
+
+    def construct_account_name(account)
+      if account.start_with?(":")
+        return @@preferences["accounts"]["prefix"]+account
+      else
+        return account
+      end
+    end
   end
 end
